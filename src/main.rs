5529f7c3cf22e175c1c0544d1fe4f5267da2a0fc
Gabriel Rosa <gabriel.edu.rosa@proton.me>
2024-11-15 16:42:00 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::{Read, Write}, net::TcpListener};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -8,8 +8,13 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+
+                let mut buf = [0; 512];
+                stream.read(&mut buf).unwrap();
+
+                stream.write(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
