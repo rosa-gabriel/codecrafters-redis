58a598098e129e6d028761f465af3496429ad1f4
Gabriel Rosa <gabriel.edu.rosa@proton.me>
2024-11-16 16:48:46 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::{
     io::{Read, Write},
     net::TcpListener,
+    usize,
 };
 
 fn main() {
@@ -22,9 +23,63 @@ fn main() {
                         }
 
                         let incoming_message = String::from_utf8_lossy(&buf);
-                        println!("{}", incoming_message);
+                        let message_parts: Vec<&str> = incoming_message.split("\r\n").collect();
 
-                        stream.write(b"+PONG\r\n").unwrap();
+                        if message_parts.len() < 1 {
+                            break;
+                        }
+
+                        let arg_count_str = message_parts.get(0).unwrap();
+
+                        if !arg_count_str.contains("*") {
+                            break;
+                        }
+
+                        let arg_count: usize =
+                            arg_count_str.replace("*", "").parse::<usize>().unwrap();
+
+                        let mut args: Vec<&str> = Vec::new();
+
+                        if arg_count > 0 {
+                            let mut curr_index: usize = 2;
+                            while curr_index != (arg_count + 1) * 2 {
+                                if message_parts.len() > curr_index {
+                                    args.push(message_parts.get(curr_index).unwrap());
+                                    curr_index += 2;
+                                } else {
+                                    println!("Error");
+                                    break;
+                                }
+                            }
+                        }
+
+                        if args.len() == 0 {
+                            break;
+                        }
+
+                        let command = args.get(0).unwrap();
+
+                        let response;
+
+                        match command.to_lowercase().as_str() {
+                            "ping" | "info" | "quit" => {
+                                response = "+PONG\r\n".to_string();
+                            }
+                            "echo" => {
+                                match args.get(1) {
+                                    Some(echo_message) => {
+                                        response = format!("${}\r\n{}\r\n", echo_message.len(), echo_message)
+                                    },
+                                    None => break,
+                                }
+                            }
+                            _ => {
+                                println!("error");
+                                break;
+                            }
+                        };
+
+                        stream.write(response.as_bytes()).unwrap();
                     }
                 });
             }
